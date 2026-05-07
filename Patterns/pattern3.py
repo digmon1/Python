1b962070f3cf5f0105c073d412dc24a102efafef
Sanjay Kumar Shrestha <xboxfamil639@gmail.com>
2026-05-07 19:32:42 +0545

    Add files via upload

---
+++ Patterns/pattern3.py
@@ -0,0 +1,8 @@
+for x in range (1,5):
+    for y in range(1,x+1):
+        print("*",end=' ')
+    print()
+for x in range (5,0,-1):
+    for y in range(1,x+1):
+        print("*",end=' ')
+    print()
---
+++ Patterns/pattern4.py
@@ -0,0 +1,10 @@
+for x in range (1,7):
+    for y in range(1,x+1):
+        print("*",end=' ')
+    print()
+for x in range (1,7):
+    for y in range(1,x+1):
+        print("*",end=' ')
+    print()
+for x in range (1,6):
+    print("*")
